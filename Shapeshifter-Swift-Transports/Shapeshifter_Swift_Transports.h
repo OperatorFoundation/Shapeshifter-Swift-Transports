1fcfcc12bb2f861784d9f7e875435c91020df251
Brandon Wiley <brandon@blanu.net>
2017-10-22 13:33:35 -0500

    Initial Commit

---
+++ Shapeshifter-Swift-Transports/Shapeshifter_Swift_Transports.h
@@ -0,0 +1,19 @@
+//
+//  Shapeshifter_Swift_Transports.h
+//  Shapeshifter-Swift-Transports
+//
+//  Created by Brandon Wiley on 10/22/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Shapeshifter_Swift_Transports.
+FOUNDATION_EXPORT double Shapeshifter_Swift_TransportsVersionNumber;
+
+//! Project version string for Shapeshifter_Swift_Transports.
+FOUNDATION_EXPORT const unsigned char Shapeshifter_Swift_TransportsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shapeshifter_Swift_Transports/PublicHeader.h>
+
+
