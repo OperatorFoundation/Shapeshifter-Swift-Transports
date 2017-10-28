d7749878dc3bd35e9fd7dd38a8a92ffd522af408
Brandon Wiley <brandon@blanu.net>
2017-10-27 20:34:24 -0500

    Added URLSession-based FakeTCPConnection

--- Shapeshifter-Swift-Transports/Testing/FakeTCPConnection.swift
+++ Shapeshifter-Swift-Transports/Testing/FakeTCPConnection.swift
@@ -9,17 +9,26 @@
 import Foundation
 import NetworkExtension
 
+func createFakeTCPConnection(to:NWEndpoint) -> FakeTCPConnection {
+    return FakeTCPConnection(to:to)!
+}
+
 class FakeTCPConnection: NWTCPConnection
 {
+    var network: URLSessionStreamTask
+    var privEndpoint: NWEndpoint
+    var privIsViable: Bool
+    var privState: NWTCPConnectionState
+    
     override var state: NWTCPConnectionState {
         get {
-            return NWTCPConnectionState.connected
+            return privState
         }
     }
     
     override var isViable: Bool {
         get {
-            return true
+            return privIsViable
         }
     }
     
@@ -31,13 +40,13 @@ class FakeTCPConnection: NWTCPConnection
     
     override var endpoint: NWEndpoint {
         get {
-            return NWHostEndpoint(hostname: "8.8.8.8", port: "1234")
+            return privEndpoint
         }
     }
     
     override var remoteAddress: NWEndpoint? {
         get {
-            return NWHostEndpoint(hostname: "8.8.8.8", port: "1234")
+            return privEndpoint
         }
     }
     
@@ -59,44 +68,87 @@ class FakeTCPConnection: NWTCPConnection
         }
     }
     
-    override func readMinimumLength(_ minimum: Int, maximumLength maximum: Int, completionHandler completion: @escaping (Data?, Error?) -> Void)
-    {
-    }
-    
-    override func readLength(_ length: Int, completionHandler completion: @escaping (Data?, Error?) -> Void) {
-    }
-    
-    override func write(_ data: Data, completionHandler completion: @escaping (Error?) -> Void)
-    {
-        completion(nil)
-    }
-    
-    override func writeClose()
-    {
-    }
-    
-    override func cancel() {
-    }
-    
     override var hasBetterPath: Bool {
         get {
             return false
         }
     }
-    
-    override init() {
-        super.init()
+
+    init?(to:NWEndpoint) {
+        privEndpoint=to
+        
+        if let hostendpoint = to as? NWHostEndpoint
+        {
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
+            network = session.streamTask(withHostName: "tcp://\(hostendpoint.hostname)", port: Int(hostendpoint.port)!)
+            network.resume()
+            
+            privIsViable=true
+            privState = .connected
+            
+            super.init()
+        } else {
+            privIsViable=false
+            privState = .disconnected
+            return nil
+        }
     }
     
-    override init(upgradeFor connection: NWTCPConnection) {
-        super.init()
+    override func readMinimumLength(_ minimum: Int, maximumLength maximum: Int, completionHandler completion: @escaping (Data?, Error?) -> Void)
+    {
+        network.readData(ofMinLength: 0, maxLength: 100000, timeout: 60) {
+            (data, bool, error) in
+            
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard data != nil else {
+                completion(nil, nil)
+                return
+            }
+            
+            completion(data, nil)
+        }
+    }
+    
+    override func readLength(_ length: Int, completionHandler completion: @escaping (Data?, Error?) -> Void) {
+        readMinimumLength(length, maximumLength: length) {
+            (data, error) in
+            
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard data != nil else {
+                completion(nil, nil)
+                return
+            }
+            
+            completion(data, nil)
+        }
+    }
+    
+    override func write(_ data: Data, completionHandler completion: @escaping (Error?) -> Void)
+    {
+        network.write(data, timeout: 0) {
+            (error) in
+
+            completion(error)
+        }
+    }
+    
+    override func writeClose()
+    {
+        network.closeWrite()
+    }
+    
+    override func cancel() {
+        writeClose()
+        network.closeRead()
+        privState = .cancelled
     }
 }
 
-func createFakeTCPConnection(to remoteEndpoint: NWEndpoint,
-                             enableTLS: Bool,
-                             tlsParameters TLSParameters: NWTLSParameters?,
-                             delegate: Any?) -> NWTCPConnection
-{
-    return FakeTCPConnection()
-}
