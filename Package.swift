dae5a8d036d78a2e89a901ca7aab2d9cf8edd570
Lita <looklita@gmail.com>
2018-10-29 13:46:02 -0500

    Dependency Updates

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "Wisp", targets: ["Wisp"]),
-        //.library(name: "Protean", targets: ["Protean"]),
+        .library(name: "Protean", targets: ["Protean"]),
         .library(name: "Wire", targets: ["Wire"]),
         .library(name: "ExampleTransports", targets: ["ExampleTransports"])
         ],
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "0.0.22"),
         .package(url: "https://github.com/OperatorFoundation/WireGuard.git", from: "0.0.5"),
         .package(url: "https://github.com/OperatorFoundation/swift-sodium", from: "0.7.1"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.12.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.13.0"),
         .package(url: "https://github.com/OperatorFoundation/HKDF.git", from: "3.0.2"),
         .package(url: "https://github.com/OperatorFoundation/Elligator.git", from: "0.1.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.3")
@@ -28,17 +28,17 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 
         .target(name: "Wisp", dependencies: ["Sodium", "CryptoSwift", "HKDF", "Elligator", "Transport", "SwiftQueue"]),
-//        .target(
-//            name: "Protean",
-//            dependencies: ["ProteanSwift", "Transport", "SwiftQueue"]),
+        .target(
+            name: "Protean",
+            dependencies: ["ProteanSwift", "Transport", "SwiftQueue"]),
         .target(
             name: "Wire",
             dependencies: ["WireGuard"]),
         .target(name: "ExampleTransports", dependencies: ["Transport"]),
         .testTarget(name: "WispTests", dependencies: ["Wisp"]),
-//        .testTarget(
-//            name: "ProteanTests",
-//            dependencies: ["Protean"]),
+        .testTarget(
+            name: "ProteanTests",
+            dependencies: ["Protean"]),
         .testTarget(name: "ExampleTransportsTests", dependencies: ["ExampleTransports"])
         ]
 )
