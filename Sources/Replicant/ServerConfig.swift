9b3a2322ca2534abeb2d316c0a74a5c5d0b91e02
Lita <looklita@gmail.com>
2018-12-20 16:33:54 -0600

    JSON Compatibility for ServerConfig

--- Sources/Replicant/ServerConfig.swift
+++ Sources/Replicant/ServerConfig.swift
@@ -11,7 +11,53 @@ import Network
 public struct ServerConfig: Codable
 {
     public let ipString: String?
-    public let portInt: NWEndpoint.Port
+    public let port: NWEndpoint.Port
+    
+    /// Creates and returns a JSON representation of the ServerConfig struct.
+    public func createJSON() -> Data?
+    {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        do
+        {
+            let serverConfigData = try encoder.encode(self)
+            return serverConfigData
+        }
+        catch (let error)
+        {
+            print("Failed to encode Server config into JSON format: \(error)")
+            return nil
+        }
+    }
+    
+    /// Checks for a valid JSON at the provided path and attempts to decode it into a server configuration file. Returns a ServerConfig struct if it is successful
+    /// - Parameters:
+    ///     - path: The complete path where the config file is located.
+    /// - Returns: The ReplicantServerConfig struct that was decoded from the JSON file located at the provided path, or nil if the file was invalid or missing.
+    static public func parseJSON(atPath path: String) -> ServerConfig?
+    {
+        let filemanager = FileManager()
+        let decoder = JSONDecoder()
+        
+        guard let jsonData = filemanager.contents(atPath: path)
+            else
+        {
+            print("\nUnable to get JSON data at path: \(path)\n")
+            return nil
+        }
+        
+        do
+        {
+            let config = try decoder.decode(ServerConfig.self, from: jsonData)
+            return config
+        }
+        catch (let error)
+        {
+            print("\nUnable to decode JSON into ServerConfig: \(error)\n")
+            return nil
+        }
+    }
 }
 
 extension String
