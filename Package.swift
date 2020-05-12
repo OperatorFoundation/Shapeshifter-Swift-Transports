aadd629bf52e6f1630966f9b5053d5dc7adad63b
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-05-12 13:19:22 -0500

    Update Dependencies

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Elligator.git", from: "0.1.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Flower.git", from: "0.0.9"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "1.1.1")
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "2.0.1")
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 swift package update
 swift package generate-xcodeproj
-rpl -R "10.10" "10.14" `basename $PWD`.xcodeproj/
+rpl -R "10.10" "10.15" `basename $PWD`.xcodeproj/
 open `basename $PWD`.xcodeproj
